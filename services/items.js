62e2412015de2f25293baa43addb8bb197476495
Andres Felipe Pardo Mesa <aanfepar@gmail.com>
2021-01-12 16:29:20 -0500

    🚀 Feat: get items using external api

--- services/items.js
+++ services/items.js
@@ -1,6 +1,27 @@
+const API_URL = " https://api.mercadolibre.com";
+const fetch = require("node-fetch");
+
 class ItemsService {
   async getItemsByQuery(query) {
-    return [];
+    return fetch(`${API_URL}/sites/MLA/search?q=${query}`).then((res) => {
+      return res.json().then((jsonRes) => {
+        const resItems = jsonRes.results.map((item) => {
+          return {
+            id: item.id,
+            title: item.title,
+            price: {
+              currency: item.currency_id,
+              amount: item.price,
+            },
+            picture: item.thumbnail,
+            condition: item.condition,
+            free_shipping: item.shipping.free_shipping,
+            sold_quantity: item.sold_quantity,
+          };
+        });
+        return { resItems };
+      });
+    });
   }
 }
 

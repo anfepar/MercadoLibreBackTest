b12de91fa25da0f9682d9b321302ae0e7d88b38b
Andres Felipe Pardo Mesa <aanfepar@gmail.com>
2021-01-12 19:58:00 -0500

    🚀 Feat: add endpoint to get a item by id

--- services/items.js
+++ services/items.js
@@ -24,6 +24,36 @@ class ItemsService {
       });
     });
   }
+
+  async getItemById(id) {
+    return fetch(`${API_URL}/items/${id}`).then((res) => {
+      return res.json().then((item) => {
+        return this.getItemDescription(id).then((itemDescription) => {
+          return {
+            id,
+            title: item.title,
+            price: {
+              currency: item.currency_id,
+              amount: item.price,
+            },
+            picture: item.secure_thumbnail,
+            condition: item.condition,
+            free_shipping: item.shipping.free_shipping,
+            sold_quantity: item.sold_quantity,
+            description: itemDescription,
+          };
+        });
+      });
+    });
+  }
+
+  async getItemDescription(id) {
+    return fetch(`${API_URL}/items/${id}/description`).then((res) => {
+      return res.json().then((jsonRes) => {
+        return jsonRes.plain_text;
+      });
+    });
+  }
 }
 
 module.exports = ItemsService;
